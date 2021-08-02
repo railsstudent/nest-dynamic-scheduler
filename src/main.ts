219dd309244c7b648bf1a1f9e551d7154ee78c32
Connie Leung <cleung334@gmail.com>
2021-08-03 04:01:37 +0800

    chore(deps): nestjs/config and nestjs/schedule
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import helmet = require('helmet')
+import * as compression from 'compression'
+import * as morgan from 'morgan'
+import * as cors from 'cors'
+import * as express from 'express'
+import { ValidationPipe } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
