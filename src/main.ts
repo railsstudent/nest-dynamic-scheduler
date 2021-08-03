622a4130cc83c17764ba604657a1c3e8ec4ec1c4
Connie Leung <cleung334@gmail.com>
2021-08-03 22:59:32 +0800

    chore: read configuration
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,20 @@ import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+  app.use(compression())
+  app.use(cors())
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false }))
+  app.use(morgan('dev'))
+  app.use(helmet())
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  )
+
+  const configService = app.get(ConfigService)
+  const port = configService.get<number>('PORT', 0)
+  await app.listen(port)
 }
 bootstrap()
