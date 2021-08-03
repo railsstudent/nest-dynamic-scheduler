8a59504676cf743f4b1ea80ff6ebd4fc88393124
Connie Leung <cleung334@gmail.com>
2021-08-03 23:30:42 +0800

    chore: read configuration
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import * as Joi from 'joi'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('development', 'production', 'test', 'provision').default('development'),
+        PORT: Joi.number().default(3001),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
