5f14f57c92c881814c49d80f685d7618bebe610d
Connie Leung <cleung334@gmail.com>
2021-08-05 08:35:32 +0800

    refactor: externalize env schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,15 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import * as Joi from 'joi'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { TaskModule } from './task'
+import { validationSchema } from './envSchema'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('development', 'production', 'test', 'provision').default('development'),
-        PORT: Joi.number().default(3001),
-      }),
+      validationSchema,
     }),
     TaskModule,
   ],
---
+++ src/envSchema.ts
@@ -0,0 +1,11 @@
+import * as Joi from 'joi'
+
+const DEFAULT_JOB_INTERVAL = '* * * * * *'
+
+export const validationSchema = Joi.object({
+  NODE_ENV: Joi.string().valid('development', 'production', 'test', 'provision').default('development'),
+  PORT: Joi.number().default(3001),
+  POST_JOB_INTERVAL: Joi.string().default(DEFAULT_JOB_INTERVAL),
+  PATCH_JOB_INTERVAL: Joi.string().default(DEFAULT_JOB_INTERVAL),
+  PUT_JOB_INTERVAL: Joi.string().default(DEFAULT_JOB_INTERVAL),
+})
