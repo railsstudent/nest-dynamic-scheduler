4807c8ceaaa0d5aa17a448eea3ec18bdaf2a7931
Connie Leung <cleung334@gmail.com>
2021-08-14 22:09:38 +0800

    chore: rename Dto class

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common'
-import { SimpleJob } from './dtos'
+import { SimpleJobDto } from './dtos'
 
 @Injectable()
 export class AppService {
-  printMessage(identifier: string, job: SimpleJob): void {
+  printMessage(identifier: string, job: SimpleJobDto): void {
     const { name, msg, timestamp } = job
     const date = new Date(timestamp)
     console.log(`${identifier}: ${name} sends ${msg} on ${date.toISOString()}`)
